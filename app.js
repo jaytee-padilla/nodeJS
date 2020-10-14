fd36ded9623fc9e1d90cc98abe941c695c198546
Jaytee <Padilla.Jaytee@gmail.com>
2020-10-13 22:22:50 -0700

    Implemented url parsing and added 2 routes

--- app.js
+++ app.js
@@ -4,11 +4,20 @@ const express = require('express');
 // stores express framework features in the 'app' variable
 const app = express();
 
-// req.body parser
-app.use(express.urlencoded({extended: false}));
+// parses url
+// express.urlencoded() is a method built into express to recognize the incoming Request Object as strings or arrays. This method is called as a middleware
+app.use(express.urlencoded({extended: true}));
+
+app.use('/add-product', (req, res, next) => {
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+
+app.post('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
 
 app.use('/', (req, res, next) => {
-  console.log('In another middleware');
   res.send('<h1>Hello from Express!</h1>');
 });
 
