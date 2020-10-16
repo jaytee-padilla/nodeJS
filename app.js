45b19d5ffa81f687183ba869ad5caa4df5ac278b
Jaytee <Padilla.Jaytee@gmail.com>
2020-10-15 21:29:37 -0700

    Added routes folder and abstracted endpoints into separate files

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const http = require('http');
 const express = require('express');
 
+// routes
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 // stores express framework features in the 'app' variable
 const app = express();
 
@@ -8,17 +12,15 @@ const app = express();
 // express.urlencoded() is a method built into express to recognize the incoming Request Object as strings or arrays. This method is called as a middleware
 app.use(express.urlencoded({extended: true}));
 
-app.use('/add-product', (req, res, next) => {
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+// the order of the routes matter
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+// if none of the other routes above are found/executed, this middleware will run
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page not found</h1>');
 });
 
-app.post('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
+app.listen(3000, () => {
+  console.log('\nServer is listening at port 3000\n');
 });
-
-app.use('/', (req, res, next) => {
-  res.send('<h1>Hello from Express!</h1>');
-});
-
-app.listen(3000);
---
+++ routes/admin.js
@@ -0,0 +1,17 @@
+// this route handles the creation of products (admin access)
+
+const express = require('express');
+
+// basically an object that stores the routes for the app
+const router = express.Router();
+
+router.get('/add-product', (req, res, next) => {
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+
+router.post('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+
+module.exports = router;
