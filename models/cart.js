321a41da4b8a688baff10d5d8c72001b5803ad9a
jaytee-padilla <padilla.jaytee@gmail.com>
2020-10-28 14:40:50 -0700

    Added ability to delete item from products page via admin-product-list

--- models/cart.js
+++ models/cart.js
@@ -8,7 +8,10 @@ module.exports = class Cart {
 	static addProduct(id, productPrice) {
 		// Fetch the previous cart
 		fs.readFile(p, (err, fileContent) => {
-			let cart = {products: [], totalPrice: 0};
+			let cart = {
+				products: [],
+				totalPrice: 0
+			};
 
 			// cart already exists
 			if (!err) {
@@ -22,12 +25,17 @@ module.exports = class Cart {
 			let updatedProduct;
 
 			if (existingProduct) {
-				updatedProduct = {...existingProduct};
+				updatedProduct = {
+					...existingProduct
+				};
 				updatedProduct.quantity = updatedProduct.quantity + 1;
 				cart.products = [...cart.products];
 				cart.products[existingProductIndex] = updatedProduct;
 			} else {
-				updatedProduct = {id: id, quantity: 1};
+				updatedProduct = {
+					id: id,
+					quantity: 1
+				};
 				cart.products = [...cart.products, updatedProduct];
 			}
 
@@ -41,5 +49,40 @@ module.exports = class Cart {
 				}
 			});
 		});
-	}
+	};
+
+	static deleteProduct(id, productPrice) {
+		fs.readFile(p, (err, fileContent) => {
+			// if there is an error trying to access/read the cart's content
+			if (err) {
+				return;
+			}
+
+			const updatedCart = {
+				...JSON.parse(fileContent)
+			};
+			const product = updatedCart.products.find(product => product.id === id);
+			const productQuantity = product.quantity;
+
+			updatedCart.products = updatedCart.products.filter(product => product.id !== id);
+			updatedCart.totalPrice = updatedCart.totalPrice - productPrice * productQuantity;
+
+			fs.writeFile(p, JSON.stringify(cart), err => {
+				if (err) {
+					console.log(err);
+				}
+			});
+		});
+	};
+
+	static getCart(cb) {
+    fs.readFile(p, (err, fileContent) => {
+      const cart = JSON.parse(fileContent);
+      if (err) {
+        cb(null);
+      } else {
+        cb(cart);
+      }
+    });
+  }
 }
--- models/product.js
+++ models/product.js
@@ -3,6 +3,9 @@
 const fs = require('fs');
 const path = require('path');
 
+// models
+const Cart = require('./cart');
+
 // get path to products.json file inside data folder
 const p = path.join(path.dirname(require.main.filename), 'data', 'products.json');
 
@@ -24,7 +27,7 @@ module.exports = class Product {
 		this.imageURL = imageURL,
 		this.price = price,
 		this.description = description
-	}
+	};
 
 	save() {
 		getProductsFromFile((products) => {
@@ -49,18 +52,32 @@ module.exports = class Product {
 				});
 			}
 		});
-	}
+	};
+
+	static deleteById(id) {
+		getProductsFromFile(products => {
+			const product = products.find(prod => prod.id === id);
+			const updatedProducts = products.filter(prod => prod.id !== id);
+
+			fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+				if (!err) {
+					// if no error, also delete the item from any users' carts
+					Cart.deleteProduct(id, product.price);
+				}
+			});
+		});
+	};
 
 	// 'static' is built into javascript
 	// it allows the fetchAll() method to be directly called on the 'Product' class itself and NOT on an instantiated object
 	static fetchAll(cb) {
 		getProductsFromFile(cb);
-	}
+	};
 
 	static findById(id, cb) {
 		getProductsFromFile(products => {
 			const product = products.find(p => p.id === id);
 			cb(product);
 		});
-	}
+	};
 };
